d6dcde6e916d34f7bf0e2109afc0ea58e5636874
afif_a_iskandar <afif_a_iskandar@telkomsel.co.id>
2023-09-15 19:36:28 +0700

    restructure model.py

--- model.py
+++ model.py
@@ -1,70 +1,48 @@
-import numpy as np
 import tensorflow as tf
-from tensorflow.keras import layers
+from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+from tensorflow.keras import layers
 from tensorflow.keras.models import load_model
-from sklearn.metrics import classification_report
 
 class SpamDetectorModel:
-    """
-    A class to build, train, and evaluate an LSTM model for spam detection.
-    """
-
-    def __init__(self, max_vocab_length, output_dim):
-        self.max_vocab_length = max_vocab_length
-        self.output_dim = output_dim
-        self.model = self.build_model()
-
-    def build_model(self):
+    def __init__(self, max_vocab_length=None, output_dim=None):
+        if max_vocab_length is not None and output_dim is not None:
+            self._build_model(max_vocab_length, output_dim)
+    
+    def _build_model(self, max_vocab_length, output_dim):
         """
-        Build and compile the LSTM model.
+        Build and compile the Keras model.
         """
-        text_vectorization = TextVectorization(max_tokens=self.max_vocab_length,
-                                               standardize="lower_and_strip_punctuation",
-                                               split="whitespace",
-                                               ngrams=None,
-                                               output_mode='int',
-                                               output_sequence_length=None)  # We'll set it later
+        self.text_vectorization = TextVectorization(max_tokens=max_vocab_length,
+                                                    standardize="lower_and_strip_punctuation",
+                                                    split="whitespace",
+                                                    output_mode='int')
         
-        embedding = layers.Embedding(input_dim=self.max_vocab_length,
-                                     output_dim=self.output_dim,
-                                     embeddings_initializer="uniform",
-                                     input_length=None)  # We'll set it later
-
+        self.embedding = layers.Embedding(input_dim=max_vocab_length,
+                                          output_dim=output_dim,
+                                          embeddings_initializer="uniform")
+        
+        self.embedding.trainable = False
+        
         inputs = layers.Input(shape=(1,), dtype='string')
-        x = text_vectorization(inputs)
-        x = embedding(x)
+        x = self.text_vectorization(inputs)
+        x = self.embedding(x)
         x = layers.LSTM(64)(x)
         outputs = layers.Dense(3, activation='softmax')(x)
-
-        model = tf.keras.Model(inputs, outputs, name="LSTM_model")
         
-        model.compile(loss='categorical_crossentropy',
-                      optimizer=tf.keras.optimizers.Adam(),
-                      metrics=["accuracy"])
-
-        return model
-
-    def train(self, X_train, y_train, X_val, y_val, epochs=5):
-        """
-        Train the model.
-        """
-        self.model.fit(X_train,
-                       y_train,
-                       epochs=epochs,
-                       validation_data=(X_val, y_val))
-
-    def evaluate_model(self, X_test, y_test):
-        """
-        Evaluate the model and print a classification report.
-        """
-        y_pred = self.model.predict(X_test)
-        y_pred_classes = np.argmax(y_pred, axis=1)
-        y_true = np.argmax(y_test, axis=1)
+        self.model = tf.keras.Model(inputs, outputs, name="LSTM_model")
         
-        print("Classification Report:")
-        print(classification_report(y_true, y_pred_classes))
-
+        self.model.compile(loss='categorical_crossentropy',
+                           optimizer=tf.keras.optimizers.Adam(),
+                           metrics=["accuracy"])
+        
+    def train(self, X_train, y_train, X_val, y_val):
+        """
+        Train the model on the provided data.
+        """
+        self.text_vectorization.adapt(X_train)
+        self.model.fit(X_train, y_train, epochs=5, validation_data=(X_val, y_val))
+    
     def save(self, filepath):
         """
         Save the model to the given filepath.
@@ -76,7 +54,6 @@ class SpamDetectorModel:
         """
         Load a saved model from the given filepath.
         """
-        loaded_model = load_model(filepath, custom_objects={'TextVectorization': TextVectorization})
         instance = cls()
-        instance.model = loaded_model
+        instance.model = load_model(filepath, custom_objects={'TextVectorization': TextVectorization})
         return instance
