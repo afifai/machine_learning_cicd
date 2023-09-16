458aa48c6ccf10d33792d4f4411aec5cc69ec74f
Afif A. Iskandar <surel.afifai@gmail.com>
2023-09-16 09:36:37 +0700

    Update model.py

--- model.py
+++ model.py
@@ -28,10 +28,10 @@ class SpamDetectorModel:
         inputs = layers.Input(shape=(1,), dtype='string')
         x = self.text_vectorization(inputs)
         x = self.embedding(x)
-        x = layers.LSTM(64)(x)
+        x = layers.GRU(64)(x)
         outputs = layers.Dense(3, activation='softmax')(x)
 
-        self.model = tf.keras.Model(inputs, outputs, name="LSTM_model")
+        self.model = tf.keras.Model(inputs, outputs, name="GRU_model")
         self.model.compile(loss='categorical_crossentropy',
                            optimizer='adam',
                            metrics=["accuracy"])
